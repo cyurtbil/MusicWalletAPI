91627b2cf356ef2a9cf16c15565d6634a6fb6df1
cyurtbil <cenky92@gmail.com>
2015-01-05 15:41:22 -0500

    now, the user can succcessfully authenticated by the provider

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,17 @@ class UsersController < ApplicationController
 
   def login
     auth_params = User.get_user_params(params)
-    binding.pry
     @user = User.new({username: auth_params[:current_user]["username"]})
     @user.image_url = auth_params[:current_user]["avatar_url"]
     @user.followers_count = auth_params[:current_user]["followers_count"]
     @user.followings_count = auth_params[:current_user]["followings_count"]
     if auth_params && @user.save
-      @authentication = Authentication.new
+      @authentication = Authentication.new({user_id: @user.id})
+      @authentication.provider = params[:provider]
+      @authentication.uid = auth_params[:current_user]["id"]
+      @authentication.access_token = auth_params[:access_token]["access_token"]
+      @authentication.scope = auth_params[:access_token]["scope"]
+      @authentication.save
       redirect_to "http://localhost:9000/#/home"
     else
       render text: "Fails to login"
