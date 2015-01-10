8950af80aa1c1f462a95b594127267353fbee32e
cyurtbil <cenky92@gmail.com>
2015-01-10 17:46:24 -0500

    added create_wallets method to the wallet model

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,21 @@
 class Wallet < ActiveRecord::Base
   has_many :songs
   belongs_to :user
+
+  def self.create_wallets(user)
+    self.new([{name: 'Dark'},
+              {name: 'Chill'},
+              {name: 'Happy'},
+              {name: 'Party'},
+              {name: 'Dance'},
+              {name: 'Sport'},
+              {name: 'Mixed Moods'},
+              {name: 'Pumped'},
+              {name: 'Sad'},
+              {name: 'Angry'},
+              {name: 'Dreamy'},
+              {name: 'Illegal'}])
+    self.all.user_id = user.id
+    self.save
+  end
 end
