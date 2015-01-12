a933625cc2ce75b9115bec2c2bbdf32550e14d1a
cyurtbil <cenky92@gmail.com>
2015-01-12 14:20:03 -0500

    added create method to songs controller and added resources to routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,19 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
+  def create
+    @song = Song.find(params[:id])
+
+    if @song.save
+      render json: @song, status: :created
+    else
+      render json: @song.errors, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def song_params
+      params.require(:song).permit(:name, :wallet_id)
+    end
 end
