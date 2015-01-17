0d870d586c1034aa74ac479db04630a2f9518da1
cyurtbil <cenky92@gmail.com>
2015-01-17 18:47:24 -0500

    edits to styles for search  bar

--- app/controllers/cloud_controller.rb
+++ app/controllers/cloud_controller.rb
@@ -1,10 +1,5 @@
 class CloudController < ApplicationController
 
-  def get_redirect_uri
-    response = Cloud.create_client
-    render json: response, status: 200
-  end
-
   def get_tracks
     tracks = Cloud.get_tracks_from_cloud(params)
     urls = Cloud.create_url_for_tracks(tracks)
